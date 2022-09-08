ecb3f967e89e397b501ae6d56be276aaec7f116e
JatinKumarParashar <vkp8015@gmail.com>
2022-09-08 15:26:05 +0530

    Task 7 ( Request/Response)

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    console.log('Jatin Kumar');
-    res.write('<head>Jatin Kumar</head>');
+    console.log(req.url);
+    if (req.url == '/home') {
+        res.write('<html>');
+        res.write('<head>Welcome home<title>My Name is Jatin Kumar</title></head>');
+        res.write('</html>');
+        res.end();
+    }
+    else if (req.url == '/about') {
+        res.write('<html>');
+        res.write('<head>Welcome to About Us page<title>My Name is Jatin Kumar</title></head>');
+        res.write('</html>');
+        res.end();
+    }
+    else if (req.url == '/node') {
+        res.write('<html>');
+        res.write('<head>Welcome to my Node Js project<title>My Name is Jatin Kumar</title></head>');
+        res.write('</html>');
+        res.end();
+    }
 });
 server.listen(4000);
