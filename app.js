604d5c7d3de8314a7c0b0e87247b29caf3370966
JatinKumarParashar <vkp8015@gmail.com>
2022-09-11 23:45:51 +0530

    Task 3 ( intro to middleware )

--- app.js
+++ app.js
@@ -1,17 +1,25 @@
 
-const express=require('express');
-const app=express();
-app.use((req,res,next)=>{
-    console.log('My name is Jatin Kumar');
-    //res.send('<h1>This is hello from express.js</h1>');
-    next();
+const express = require('express');
+const app = express();
+const bodyParsed = require('body-parser');
 
-});
-app.use((req,res,next)=>{
-    console.log('This is another app.use');
-    res.send('<h1>hello from another app.use</h1>');
-   // res.send( { 'key1': 'value' })
-    next();
+
+app.use(bodyParsed.urlencoded({ extended: false }));
+
+app.use('/add-product', (req, res, next) => {
+    res.send('<html><body><form action="/product" method="POST"><lable>Product:</lable><input type="text" name="title"/><lable>Size:</lable><input type="size" name="number"/><button type="submit">Add Product</button></form></body></html>');
+
 })
-      
+
+app.use('/product', (req, res, next) => {
+    
+    console.log(req.body);
+    
+    res.redirect('/');
+})
+app.use('/', (req, res, next) => {
+    res.send('<h1>This is hello from express.js</h1>');
+});
+
+
 app.listen(3000);
