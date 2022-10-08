91c03d82c8d1ffaade7b2b1d5094af23a9527ddf
JatinKumarParashar <vkp8015@gmail.com>
2022-10-08 10:34:04 +0530

    Task 2 ( Ecommerce Website )

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
 const btn = document.getElementById("btn");
-const nav = document.getElementById("nav");
+const container=document.getElementById('container');
 
 btn.addEventListener("click", () => {
-    nav.classList.toggle("active");
-    btn.classList.toggle("active");
+    creatNotification();
 });
+
+function creatNotification(){
+const notif=document.createElement('div');
+notif.classList.add('toast');
+notif.innerText='This is Notification';
+container.appendChild(notif);
+setTimeout(()=>{
+    notif.remove();
+},3000);
+}
