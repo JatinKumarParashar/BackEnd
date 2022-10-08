eafc984422c5789b757d90e9c80c367606063f8a
JatinKumarParashar <vkp8015@gmail.com>
2022-10-08 16:11:09 +0530

    Task 6 (Ecomerce Website)

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
-const toggle=document.getElementById('toggle');
- toggle.addEventListener('change',(e)=>{
-    document.body.classList.toggle('dark',e.target.checked);
- })
+const imgs=document.getElementById('imgs');
+
+const img=document.querySelectorAll('#imgs img');
+let index=0;
+function carousel(){
+index++;
+if(index>=img.length){
+    index=0;
+}
+imgs.style.transform=`translateX(${-index*500}px)`;
+}
+setInterval(carousel,1000);
